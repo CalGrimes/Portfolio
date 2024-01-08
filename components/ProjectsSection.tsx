130f12a981666e59bc96792cacdf8daf0c9c0bea
CalGrimes <cal.grimes@outlook.com>
2024-01-08 20:59:22 +0000

    Update ProjectsSection.tsx

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -38,7 +38,7 @@ const ProjectsSection = () => {
                     </Link>
                   </div>
                   <div className="md:w-1/2">
-                    <h1 className="text-4xl font-bold mb-3">{project.name}</h1>
+                    <h1 className="text-2xl font-bold mb-3">{project.name}</h1>
                     <p className="text-xl leading-7 mb-2 text-neutral-600 dark:text-neutral-400">
                       {project.description}
                     </p>
