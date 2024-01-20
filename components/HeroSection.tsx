96699bb4f87808d4cf730a8b0146701feb39cd52
CalGrimes <cal.grimes@outlook.com>
2024-01-20 22:22:01 +0000

    [refactor] Sizing changes for smaller screens

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -10,19 +10,19 @@ const HeroSection: React.FC = () => {
 
 return (
     <section id="home" className="max-w-full">
-      <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 pt-16 sm:pt-16 sm:pb-10 md:pt-16 md:pb-6 md:flex-row md:space-x-4 md:text-left gap-24">
-        <div className="md:mt-2">
+      <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 pt-16 sm:pt-16 sm:pb-10 lg:pt-16 lg:pb-6 lg:flex-row lg:space-x-4 lg:text-left gap-12 lg:gap-24">
+        <div className="lg:mt-2">
         <Image
           src={"/headshot.png"}
           alt=""
           width={325}
           height={100}
-          className="rounded-full shadow-2xl headshot object-cover max-h-80 md:max-h-full object-top"
+          className="rounded-full shadow-2xl headshot object-cover max-h-80 lg:max-h-full object-top"
         />
         </div>
-        <div className="md:mt-2 md:w-3/5">
-          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl cursor-default">Hi, I&#39;m Cal!</h1>
-          <p className="text-lg mt-4 mb-6 md:text-2xl cursor-default">
+        <div className="lg:mt-2 lg:w-3/5">
+          <h1 className="text-4xl font-bold mt-6 lg:mt-0 lg:text-7xl cursor-default">Hi, I&#39;m Cal!</h1>
+          <p className="text-lg mt-4 mb-6 lg:text-2xl cursor-default">
             I&#39;m a{" "}
             <span className={`"font-semibold dark:text-amber-400 text-purple-800`}>
                 Software Engineer{" "}
@@ -30,7 +30,7 @@ return (
             working on personalised solutions for the modern world.
           </p>
           <Link
-              to="projects"
+              to="section-headers"
               className={`font-semibold px-6 py-3 rounded shadow cursor-pointer dark:bg-amber-400  dark:text-black bg-purple-800 text-white 0 hover:scale-105 ease-in duration-75 hover:border-1 border-black`}
               activeClass="active"
               spy={true}
@@ -38,7 +38,7 @@ return (
               offset={-100}
               duration={500}
           >
-            Projects
+            Explore
           </Link>
           <Certifications />
           <SkillsSection />
@@ -47,7 +47,7 @@ return (
       </div>
       <div className="flex flex-row items-center text-center justify-center ">
         <Link
-          to="skills"
+          to="section-headers"
           activeClass="active"
           spy={true}
           smooth={true}
