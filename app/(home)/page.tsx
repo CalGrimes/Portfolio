cbca2416e668c3ef3e8f0dc68aef4ea12d0b9707
CalGrimes <cal.grimes@outlook.com>
2024-01-20 22:23:35 +0000

    [refactor] Navbar item changes
    Swapped to order of page scroll
    Added resume

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,16 +1,16 @@
 import AboutSection from "@/components/AboutSection"
 import HeroSection from "@/components/HeroSection"
-import ProjectsSection from "@/components/ProjectsSection"
+import ProjectsSection from "@/components/ProjectsSectionOld"
 import ExperienceSection from "@/components/ExperienceSection";
 import Contact from "@/components/Contact";
 import SkillsSection from "@/components/SkillsSection";
+import SectionHeader from "@/components/SectionHeader";
 export default function Home() {
   return (
     <main className="mx-auto px-4 sm:px-6">
       <HeroSection />
-      <ExperienceSection />
+      <SectionHeader />
       <AboutSection />
-      <ProjectsSection />
       <Contact />
     </main>
   )
