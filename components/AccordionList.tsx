4bf3cf269f25a13e1cb741740360ccb0ccef6a3a
CalGrimes <cal.grimes@outlook.com>
2024-01-20 22:21:24 +0000

    [refactor] Accordion style changes

--- components/AccordionList.tsx
+++ components/AccordionList.tsx
@@ -22,19 +22,19 @@ import {
 
 export const AccordionList = ({ items }: AccordionListProps) => {
     return (
-         <Accordion allowToggle className={`rounded-xl border p-2 shadow-xl border-black dark:border-white`}>
+         <Accordion allowToggle className={`rounded-xl  p-2 shadow-xl`}>
             {items.map((item, index) => (
                 <AccordionItem key={index}>
                     <h2>
                     <AccordionButton className='sm:hover:scale-100 hover:scale-x-105 ease-in duration-75'>
                             <Box as="span" flex='1' textAlign='left' fontWeight='bold' className='p-2'>
-                                <p className='rounded border border-xl border-black dark:bg-amber-400 bg-purple-800 text-white dark:text-black px-2 inline-block'>{item.title}</p>
+                                <p className='px-2 inline-block'>{item.title}</p>
                             </Box>
                             <AccordionIcon />
                         </AccordionButton>
                     </h2>
                     {/* <hr className={`rounded border my-2 border-black dark:border-white`} /> */}
-                    <AccordionPanel pb={4} className='ml-2 px-2 rounded bg-gray-200 dark:bg-zinc-800 text-justify'>
+                    <AccordionPanel pb={4} className='ml-2 px-2 rounded  text-justify'>
                         {item.content}
                     </AccordionPanel>
                 </AccordionItem>
