9b5b5a60ce1e09ece7fc86de7b297900d27048d9
CalGrimes <cal.grimes@outlook.com>
2024-01-20 22:25:39 +0000

    [refactor] Mobile optimisation
    Colour adjustments to Tags

--- components/TagNoLink.tsx
+++ components/TagNoLink.tsx
@@ -9,7 +9,7 @@ export default function TagNoLink({ tag, index}: Props) {
 
   return (
     <p key={index}
-    className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 dark:bg-zinc-800 dark:text-gray-200 rounded font-semibold cursor-default"
+    className="bg-gray-300 px-4 py-2 mr-2 mt-2 text-gray-800 dark:bg-zinc-800 dark:text-gray-200 rounded font-semibold cursor-default"
   >
     {tag}
   </p>
