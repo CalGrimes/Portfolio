42385cc6e0f9fa5fb4cbabd4a41ecab21c32291d
CalGrimes <cal.grimes@outlook.com>
2024-03-03 20:15:42 +0000

    [feat] New Article Style

--- components/Article.tsx
+++ components/Article.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 import { Author } from '@/components/Author';
+import { Text } from '@chakra-ui/react';
+import { useState } from 'react';
 
 interface ArticleProps {
   post: {
@@ -10,11 +12,8 @@ interface ArticleProps {
       category: string;
       title: string;
       excerpt: string;
-      author: string;
-    };
-    previewUrl: string;
-    category: {
-      href: string;
+      author: any;
+      url: string;
     };
   };
 }
@@ -24,44 +23,49 @@ const formatDate = (date: string) => {
     return new Date(date).toLocaleDateString(undefined, options);
   }
 
-const Article: React.FC<ArticleProps> = ({ post }) => (
-  <article key={post.data.id} className="flex flex-col items-start">
-    <div className="relative w-full hover:scale-105 ease-in-out transition-all duration-300">
-      <a href={post.previewUrl}>
-        <Image
-          src={post.data.thumbnail}
-          alt=""
-          width={500}
-          height={500}
-          className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
-        />
-        <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
-      </a>
-    </div>
-    <div className="max-w-xl">
-      <div className="mt-8 flex items-center gap-x-4 text-xs">
-        <time dateTime={post.data.date} className="text-gray-500 dark:text-gray-300">
-          {formatDate(post.data.date)}
-        </time>
-        <a
-          href="{post.category.href}"
-          className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-        >
-          {post.data.category}
-        </a>
-      </div>
-      <div className="group relative">
-        <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 dark:text-gray-100 group-hover:text-gray-600 dark:group-hover:text-gray-400">
-          <a href={post.previewUrl}>
+  const Article: React.FC<ArticleProps> = ({ post }) => {
+    return (
+      <article
+        key={post.data.id}
+        className="relative isolate flex flex-col overflow-hidden rounded-2xl bg-gray-900 p-8 group h-96"
+      >
+        <img src={post.data.thumbnail} alt="" className="absolute inset-0 -z-10 h-full w-full object-cover" />
+        <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40" />
+        <div className="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+  
+        <div className="flex flex-wrap items-center gap-y-1 overflow-hidden text-sm leading-6 text-gray-300 mt-auto">
+          <time dateTime={post.data.date} className="mr-8">
+            {formatDate(post.data.date)}
+          </time>
+          <div className="-ml-4 flex items-center gap-x-4">
+              {post.data.author && (
+                <>
+            <svg viewBox="0 0 2 2" className="-ml-0.5 h-0.5 w-0.5 flex-none fill-white/50">
+              <circle cx={1} cy={1} r={1} />
+            </svg>
+            <div className="flex gap-x-2.5">
+                <>
+                  <img src={post.data.author?.value.data.photo} alt="" className="h-6 w-6 flex-none rounded-full bg-white/10" />
+                  {post.data.author?.value.data.fullName}
+                </>
+            </div>
+            </>
+              )}
+          </div>
+        </div>
+        <div className="mt-3 text-lg font-semibold leading-6 text-white">
+          <a href={post.data.url}>
             <span className="absolute inset-0" />
             {post.data.title}
           </a>
-        </h3>
-        <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600 dark:text-gray-300">{post.data.excerpt}</p>
-      </div>
-      {post.data.author && <Author author={post.data.author} />}
-    </div>
-  </article>
-);
+          <Text 
+    className='text-sm text-gray-300 dark:text-gray-400 overflow-hidden relative z-10 flex-grow truncate group-hover:whitespace-normal group-hover:overflow-visible'
+>
+    {post.data.excerpt}
+</Text>
+        </div>
+      </article>
+    );
+  }
 
 export default Article;
