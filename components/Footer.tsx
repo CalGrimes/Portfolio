b58051d2d0bfa37b92dd9d21340707990fddba02
CalGrimes <cal.grimes@outlook.com>
2024-03-05 22:00:28 +0000

    [chore] footer spacing

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ import { FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <footer className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
+    <footer className=" px-4 sm:px-6 ">
       <hr className="w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0"></hr>
       <div className="mx-auto  p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between">
         <div className="flex flex-row items-center justify-center space-x-1 text-neutral-500 dark:text-neutral-100">
