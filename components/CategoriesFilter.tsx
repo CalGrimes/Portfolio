dda3501a27387ce6e6957888b85661cf1d4c9969
CalGrimes <cal.grimes@outlook.com>
2024-03-09 12:46:08 +0000

    [chore] remove fetching internal categories

--- components/CategoriesFilter.tsx
+++ components/CategoriesFilter.tsx
@@ -21,60 +21,25 @@ import React, { useEffect, useState } from 'react'
   
   // Then use this new type in your component
   export default function CategoriesFilter({onFilter}: {onFilter: any}) {
-      const [categories, setCategories] = useState<CategoryContent[]>([]);
+    const [categories, setCategories] = useState<CategoryContent[]>([]);
     const [category, setCategory] = useState<string | null>(null);
     const [loading, setLoading] = useState(true);
 
-    // Push states to the URL
-    useEffect(() => {
-        const searchParams = new URLSearchParams(window.location.search);
-
-        if (!category) {
-            searchParams.delete('category');
-        }
-        else
-        {
-            searchParams.set('category', category);
-        }
-
-        window.history.pushState({}, '', `${window.location.pathname}?${searchParams.toString()}`);
-    }, [category]);
-
-    // Fetch url search params to update states
-    useEffect(() => {
-        const searchParams = new URLSearchParams(window.location.search);
-        const category = searchParams?.get('category');
-        if (category) {
-            setCategory(category);
-        }
-    }
-    , []);
-
     useEffect(() => {
         const fetchCategories = async () => {
-            const categoriesInternal = await builder.getAll(
-                'blog-article', {
-                    options: {
-                        includeUnpublished: process.env.NEXT_PUBLIC_ENVIRONMENT == 'development' ? true : false,
-                        includeRefs: true
-                      },
-                    fields: 'data.category',
-            });
             const categoriesExternal = await builder.getAll(
                 'external-articles', {
                     options: {
                         includeUnpublished: process.env.NEXT_PUBLIC_ENVIRONMENT == 'development' ? true : false,
                         includeRefs: true
-                      },
+                    },
                     fields: 'data.category',
             });
 
             // merge and remove duplicates
-            let allCategories = Array.from(new Set([...categoriesInternal, ...categoriesExternal]));  
+            let allCategories = Array.from(new Set(categoriesExternal));  
             let unqCategories = allCategories.filter((v, i, a) => a.findIndex(t => (t.data?.category === v.data?.category)) === i);
-
-
-
+            
             setCategories(unqCategories as any);
             setLoading(false);
         }
