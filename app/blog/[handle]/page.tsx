2a900786cc83041b91ea36f70b1dfe35a0b57992
CalGrimes <cal.grimes@outlook.com>
2024-03-09 13:08:05 +0000

    [chore] Remove  internal content rendering

--- app/blog/[handle]/page.tsx
+++ app/blog/[handle]/page.tsx
@@ -1,57 +1,65 @@
 import React from "react";
-import { BuilderContent, builder } from '@builder.io/sdk'
-import { RenderBuilderContent } from "@/components/builder";
-// See the full code: https://www.builder.io/c/docs/integrate-section-building?codeFramework=nextApp#add-an-announcement-bar-section-to-your-app
+import { notFound } from "next/navigation";
+// import { BuilderContent, builder } from '@builder.io/sdk'
+// import { RenderBuilderContent } from "@/components/builder";
+// // See the full code: https://www.builder.io/c/docs/integrate-section-building?codeFramework=nextApp#add-an-announcement-bar-section-to-your-app
 
 
-// Replace with your Public API Key
-builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
+// // Replace with your Public API Key
+// builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
 
 
-export async function generateMetadata({ params }: any) {
-  const content = await builder
-  .get("blog-article", {
-    // Include references, like our `author` ref
-    options: { includeRefs: true },
-    query: {
-      // Get the specific article by handle
-      "data.handle": params.handle,
-    },
-    cacheSeconds: 5,
-    prerender: false
-  })
-  .promise() || null;
+// export async function generateMetadata({ params }: any) {
+//   const content = await builder
+//   .get("blog-article", {
+//     // Include references, like our `author` ref
+//     options: { includeRefs: true },
+//     query: {
+//       // Get the specific article by handle
+//       "data.handle": params.handle,
+//     },
+//     cacheSeconds: 5,
+//     prerender: false
+//   })
+//   .promise() || null;
 
 
-  return {
-    // title: content.data.title,
-    // description: content.data.excerpt,
-  }
-}
+//   return {
+//     // title: content.data.title,
+//     // description: content.data.excerpt,
+//   }
+// }
+
+// export default async function Page({params}:any) {
+
+//   const content = await builder
+//   .get("blog-article", {
+//     // Include references, like our `author` ref
+//     options: { includeRefs: true },
+//     query: {
+//       // Get the specific article by handle
+//       "data.handle": params.handle,
+//     },
+//     cacheSeconds: 5,
+//     prerender: false
+//   })
+//   .promise() || null;
+
+//   if (!content) {
+//     return <div>Not found</div>
+//   }
+
+
+//   return (
+//     <div className="pt-24 h-full">
+//       <RenderBuilderContent content={content} key={content.id}  />
+//     </div>
+//   );
+// }
+
 
 export default async function Page({params}:any) {
 
-  const content = await builder
-  .get("blog-article", {
-    // Include references, like our `author` ref
-    options: { includeRefs: true },
-    query: {
-      // Get the specific article by handle
-      "data.handle": params.handle,
-    },
-    cacheSeconds: 5,
-    prerender: false
-  })
-  .promise() || null;
+  return notFound();
 
-  if (!content) {
-    return <div>Not found</div>
-  }
-
-
-  return (
-    <div className="pt-24 h-full">
-      <RenderBuilderContent content={content} key={content.id}  />
-    </div>
-  );
 }
