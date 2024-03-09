3150bcd1f9ababff4d86b8f76b64f8aee020ccf4
CalGrimes <cal.grimes@outlook.com>
2024-03-09 13:40:21 +0000

    [refactor] Blog page exports

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -11,9 +11,9 @@ import { Input, Skeleton } from "@chakra-ui/react";
 
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
 
-const ARTICLES_PER_PAGE = 30;
+export const ARTICLES_PER_PAGE = 30;
 
-const getTotalArticles = async (category: string | null, searchQuery: string) => {
+async function getTotalArticles(category: string | null, searchQuery: string) {
   const totalExternalArticles = await builder.getAll(
     "external-articles",
     {
@@ -34,15 +34,11 @@ const getTotalArticles = async (category: string | null, searchQuery: string) =>
           ]
         },
         cacheSeconds: 5,
-        prerender: false
     }
   ).then((res) => { return res.length });
 
-
-
   // round up to the nearest whole number
   const total = Math.ceil(totalExternalArticles / ARTICLES_PER_PAGE);
-
   return total;
 }
 
@@ -130,31 +126,7 @@ export default function Page({params}:any) {
   }, [category, currentPage, searchQuery]);
 
   if (loading) {
-    return (
-      <div className="container mx-auto px-4 pt-32">
-        <div className="text-center">
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl">Blog Postings</h2>
-            <p className="mt-2 text-lg leading-8 text-gray-600 dark:text-gray-400">
-              Thank you for visiting my blog. I hope you find the information useful.
-            </p>
-          </div>
-        <div className="mt-20 flex space-x-4">
-          <Input placeholder="Search articles" className='rounded-lg text-black' />
-          <select className='rounded-md text-black'>
-              <option value=''>All Categories</option>
-          </select>
-        </div>
-        <div className="mt-20 grid gap-8 md:grid-cols-2 lg:grid-cols-3">
-          {Array.from({ length: 5 }).map((_, index) => (
-              <article key={index} className="flex flex-col items-start justify-between bg-gray-200 dark:bg-stone-800/70">
-                <Skeleton height="384px" fadeDuration={4} width="100%" />
-                <Skeleton height="20px" fadeDuration={4} width="80%" />
-                <Skeleton height="20px" fadeDuration={4} width="60%" />
-              </article>
-            ))}
-            </div>
-      </div>
-    )
+    return <LoadingPage />;
   }
   return (
       <div className="container mx-auto px-4 pt-16 space-y-12">
@@ -182,6 +154,34 @@ export default function Page({params}:any) {
         </div>
         <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={setCurrentPage} />
 
+      </div>
+  )
+}
+
+function LoadingPage() {
+  return (
+    <div className="container mx-auto px-4 pt-32">
+        <div className="text-center">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl">Blog Postings</h2>
+            <p className="mt-2 text-lg leading-8 text-gray-600 dark:text-gray-400">
+              Thank you for visiting my blog. I hope you find the information useful.
+            </p>
+          </div>
+        <div className="mt-20 flex space-x-4">
+          <Input placeholder="Search articles" className='rounded-lg text-black' />
+          <select className='rounded-md text-black'>
+              <option value=''>All Categories</option>
+          </select>
+        </div>
+        <div className="mt-20 grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+          {Array.from({ length: 5 }).map((_, index) => (
+              <article key={index} className="flex flex-col items-start justify-between bg-gray-200 dark:bg-stone-800/70">
+                <Skeleton height="384px" fadeDuration={4} width="100%" />
+                <Skeleton height="20px" fadeDuration={4} width="80%" />
+                <Skeleton height="20px" fadeDuration={4} width="60%" />
+              </article>
+            ))}
+            </div>
       </div>
   )
 }
