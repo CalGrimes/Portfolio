8128ce7eb9de77da65dc98e6878fc3a2e5bbbcfa
Cal Grimes <cal.grimes@outlook.com>
2026-05-08 12:12:23 +0100

    fix builder.tsx build error

--- components/builder.tsx
+++ components/builder.tsx
@@ -7,12 +7,13 @@ import DefaultErrorPage from "next/error";
 
 interface BuilderPageProps { 
   content?: BuilderContent;
+  model: string;
 }
 
 // Replace with your Public API Key
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
 
-export function RenderBuilderContent({ content }: BuilderPageProps) { 
+export function RenderBuilderContent({ content, model }: BuilderPageProps) { 
   // Call the useIsPreviewing hook to determine if 
   // the page is being previewed in Builder
   const isPreviewing = useIsPreviewing(); 
@@ -20,7 +21,7 @@ export function RenderBuilderContent({ content }: BuilderPageProps) {
   // render the BuilderComponent with the specified content and model props.
   if (content || isPreviewing) {
     return (
-    <BuilderComponent content={content} model="blog-article" />
+    <BuilderComponent content={content} model={model} />
     );
   }
   // If the `content` is falsy and the page is 
